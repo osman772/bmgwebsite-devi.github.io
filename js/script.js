256decf6fe8e3831ba7ff6d78faa412ef89e3cb7
osman772 <osman.polaat.0@gmail.com>
2025-12-14 18:55:45 +0300

    V1

---
+++ js/script.js
@@ -0,0 +1,16 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    if (name === "" || email === "" || message === "") {
+        alert("Boş geçilemez!");
+    } else {
+        alert("Mesaj gönderildi!");
+        clear();
+    }
+
+
+ 
+});
